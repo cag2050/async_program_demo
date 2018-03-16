45e458bc9e659b778a0bf0da7b20c2a945b4b3e8
cag2050 <cag2050@163.com>
2018-03-16 11:47:28 +0800

    m

--- src/readfile-async.js
+++ src/readfile-async.js
@@ -3,14 +3,20 @@ const fs = require('fs')
 var readFile = function (filePath) {
     return new Promise((resolve, reject) => {
         fs.readFile(filePath, 'utf8', (error, data) => {
-            if(error) reject(error)
+            if (error) {
+                console.log('error')
+                console.log(error)
+                reject(error)
+            }
             resolve(data)
         })
     })
 }
 
 var asyncReadFile = async function () {
-    var file1 = await readFile('/etc/networks')
+    // var file1 = await readFile('/etc/networks')
+    // var file2 = await readFile('/etc/shells')
+    var file1 = await readFile('3.txt')
     var file2 = await readFile('/etc/shells')
     console.log(file1.toString())
     console.log('=======')
